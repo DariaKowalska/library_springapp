44480c8082cea09325f7a1d389775b3e2910a881
Daria Peziak-Kowalska <d.peziak.kowalska@gmail.com>
2019-10-24 10:59:04 +0200

    Initial commit

---
+++ src/main/java/com/myproject/library_springapp/LibrarySpringappApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.library_springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySpringappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySpringappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/library_springapp/LibrarySpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.library_springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
